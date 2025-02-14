6ebc3bf92eabafa02312229156ef2cc5090f45a8
Nicholas Pisarro <infinityminusnine@gmail.com>
2025-02-14 12:52:28 -0500

    reformat, replace numbers with constants

--- src/animation.cpp
+++ src/animation.cpp
@@ -1,8 +1,7 @@
 #include "animation.h"
 
-#include <stdint.h>
-
 #include <FastLED.h>
+#include <stdint.h>
 
 #include "constants.h"
 #include "util.h"
@@ -22,8 +21,8 @@ constexpr uint8_t BACK_FADE = 230;
 
 constexpr BlendMode BLEND_MODE = BlendMode::BLEND_87_5;
 
-
-bool animation_update(uint8_t brightness[], uint16_t roll, bool click, bool reset) {
+bool calculate_next_frame(uint8_t brightness[], uint16_t roll, bool click,
+                          bool reset) {
   static uint32_t directionRegister = 0;
   static uint8_t sparking = DEFAULT_SPARKING;
 
@@ -37,23 +36,28 @@ bool animation_update(uint8_t brightness[], uint16_t roll, bool click, bool rese
 
   static uint32_t lastForcedSpark = 0;
   static uint32_t wokeAgo = 0;
+  constexpr uint32_t WOKE_AGO_TIMEOUT = 2500;
+  constexpr uint32_t FORCED_SPARK_LEVEL1 = 400 * 6;
+  constexpr uint32_t FORCED_SPARK_LEVEL2 = 5000 * 6;
+  constexpr uint32_t FORCED_SPARK_LEVEL3 = 60000;
   bool sparkNow = false;
+  uint32_t now = millis();
   if (click || reset) {
     sparkNow = true;
-    lastForcedSpark = millis();
+    lastForcedSpark = now;
     if (reset) {
       wokeAgo = 0;
     }
-  } else if (millis() - lastForcedSpark < 400) {
+  } else if (now - lastForcedSpark < FORCED_SPARK_LEVEL1) {
     sparkNow = true;
-  } else if (millis() - wokeAgo < 2500) {
+  } else if (now - wokeAgo < WOKE_AGO_TIMEOUT) {
     sparkNow = true;
-  } else if (millis() - lastForcedSpark < 5000) {
-    sparking = map(millis() - lastForcedSpark, 400, 5000, 120, 50);
-    //    sparking = 120;
-  } else if (millis() - lastForcedSpark < 10000) {
-    sparking = map(millis() - lastForcedSpark, 5000, 10000, 50, 10);
-    //    sparking = 50;
+  } else if (now - lastForcedSpark < FORCED_SPARK_LEVEL2) {
+    sparking = map(now - lastForcedSpark, FORCED_SPARK_LEVEL1,
+                   FORCED_SPARK_LEVEL2, 120, 50);
+  } else if (now - lastForcedSpark < FORCED_SPARK_LEVEL3) {
+    sparking = map(now - lastForcedSpark, FORCED_SPARK_LEVEL2,
+                   FORCED_SPARK_LEVEL3, 50, 10);
   } else {
     // sleep now
     return true;
@@ -64,14 +68,15 @@ bool animation_update(uint8_t brightness[], uint16_t roll, bool click, bool rese
 
   // Step 1.  Cool down every cell a little
   for (uint8_t i = 0; i < NUM_LEDS; i++) {
-    //    heat[i] = qsub8(heat[i],  random8(0, ((((uint16_t) lerp8by8(MIN_COOLING, MAX_COOLING, currentCooling)) * 10) / NUM_LEDS) + 2));
+    //    heat[i] = qsub8(heat[i],  random8(0, ((((uint16_t)
+    //    lerp8by8(MIN_COOLING, MAX_COOLING, currentCooling)) * 10) / NUM_LEDS)
+    //    + 2));
     heat[i] = qsub8(heat[i], random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
   }
 
   // right side
   for (uint8_t k = 0; k < NUM_LEDS; ++k) {
     if (!bitRead(directionRegister, k)) {
-
       uint8_t neighbor1 = addmod8(k, 1, NUM_LEDS);
       neighbor1 = !bitRead(directionRegister, neighbor1) ? heat[neighbor1] : 0;
 
@@ -79,7 +84,9 @@ bool animation_update(uint8_t brightness[], uint16_t roll, bool click, bool rese
       neighbor2 = !bitRead(directionRegister, neighbor2) ? heat[neighbor2] : 0;
 
       //      heat[k] = (((uint16_t) neighbor1) + neighbor2 + neighbor2) / 3;
-      heat[k] = lerp8by8(heat[k], (((uint16_t)neighbor1) + neighbor2 + neighbor2) / 3, BACK_FADE);
+      heat[k] =
+          lerp8by8(heat[k], (((uint16_t)neighbor1) + neighbor2 + neighbor2) / 3,
+                   BACK_FADE);
     }
   }
 
@@ -93,15 +100,17 @@ bool animation_update(uint8_t brightness[], uint16_t roll, bool click, bool rese
       neighbor2 = bitRead(directionRegister, neighbor2) ? heat[neighbor2] : 0;
 
       //      heat[k] = (((uint16_t) neighbor1) + neighbor2 + neighbor2) / 3;
-      heat[k] = lerp8by8(heat[k], (((uint16_t)neighbor1) + neighbor2 + neighbor2) / 3, BACK_FADE);
+      heat[k] =
+          lerp8by8(heat[k], (((uint16_t)neighbor1) + neighbor2 + neighbor2) / 3,
+                   BACK_FADE);
     }
   }
 
-  // to involve pitch: weighted random that diffusion goes [heat - 2] * 2 vs. [heat + 1] etc.
+  // to involve pitch: weighted random that diffusion goes [heat - 2] * 2 vs.
+  // [heat + 1] etc.
 
   // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
   if (sparkNow || random8() < sparking) {
-
     uint8_t y = random8(NUM_LEDS / 6);
     if (random8() <= 160) {
       y = addmod8(bottomLed, y, NUM_LEDS);
@@ -119,7 +128,6 @@ bool animation_update(uint8_t brightness[], uint16_t roll, bool click, bool rese
 
   return false;
 }
-
 
 void update_led_pattern(uint8_t brightness[]) {
   static uint8_t phase = 0;
@@ -178,15 +186,15 @@ void blendIt(BlendMode blendMode, uint8_t& brightness_ref, uint8_t new_val) {
       brightness_ref = (brightness_ref >> 2) + (new_val >> 1) + (new_val >> 2);
       break;
     case BlendMode::BLEND_87_5:
-      brightness_ref = (brightness_ref >> 3) + (new_val >> 1) + (new_val >> 2) + (new_val >> 3);
+      brightness_ref = (brightness_ref >> 3) + (new_val >> 1) + (new_val >> 2) +
+                       (new_val >> 3);
       break;
   }
 }
 
-
 // test whether the accelerometer works
-bool accel_test_animation(uint8_t brightness[], uint16_t roll, bool click, bool reset) {
-
+bool angle_test_animation(uint8_t brightness[], uint16_t roll, bool click,
+                          bool reset) {
   uint8_t bottomLed = map(roll, 0, 0xffff, 0, NUM_LEDS);
 
   for (uint8_t i = 0; i < NUM_LEDS; ++i) {
